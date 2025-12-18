5e07a62ebafeea6f01efc8b52c56d62b87c5bc86
NotGibran <64770138+NotGibranID@users.noreply.github.com>
2025-12-18 09:47:49 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+just a website with video hub
---
+++ script.js
@@ -0,0 +1,18 @@
+fetch('https://json.ngid.my.id/videos.json')
+  .then(res => res.json())
+  .then(data => {
+    // Sort by id descending
+    data.sort((a, b) => b.id - a.id);
+
+    const container = document.getElementById('videoList');
+    data.forEach(video => {
+      const div = document.createElement('div');
+     div.innerHTML = `
+        <iframe src="${video.url}" title="${video.title}" allowfullscreen></iframe>
+        <p>${video.title}</p>
+        <p>Made by: ${video.madeby}</p> 
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => console.error(err));
